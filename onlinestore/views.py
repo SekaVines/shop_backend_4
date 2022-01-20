9ba6e2bcf5a7a6b7a861de4977c6957bb9fda841
Elcycyi <aazamzunusov039@gmail.com>
2022-01-20 20:57:29 +0600

    homework_3

--- onlinestore/views.py
+++ onlinestore/views.py
@@ -22,15 +22,33 @@ def product_list_view(request):
 #     return Response(data=data)
 
 
-@api_view(['GET'])
-def product_detail_view(request):
-    product.title = request.data['title']
-    product.description = request.data.get('description', '')
-    product.price = request.data['price']
-    product.category_id = request.data['category']
-    product.tags.set(request.data['tags'])
-    product.save()
-    return Response(data={'massage': 'Product updated!'})
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail_view(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={'error': 'Product not found!'})
+    if request.method == 'GET':
+        data = ProductDetailSerializer(product, many=False).data
+        return Response(data=data)
+    elif request.method == 'PUT':
+        serializer = ProductUpdateValidateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE,
+            data={'message': 'error',
+            'errors': serializer.errors})
+        product.title = request.data['title']
+        product.description = request.data.get('description', '')
+        product.price = request.data['price']
+        product.category_id = request.data['category']
+        product.tags.set(request.data['tags'])
+        product.save()
+        return Response(data={'massage': 'Product updated!'})
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(data={'message': 'Product successfully removed!'})
+
 
 
 @api_view(['GET'])
